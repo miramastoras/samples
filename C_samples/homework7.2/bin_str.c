f760e9354fdc02ef3b5c67f236695b8632dc62e3
Mira Mastoras <mastorasmira@gmail.com>
2019-01-03 11:28:07 -0800

    Add files via upload

---
+++ C_samples/homework7.2/bin_str.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <memory.h>
+
+// take array and do recursion onto it
+
+void print_ar(char* ar, int len);
+void _rec_bin_string(int lenBinStr, char* binarystring, char* cur_string, int index);
+
+
+
+void rec_bin_string(int lenBinStr, char* binarystring) {
+  char* cur_string = (char*)malloc((lenBinStr + 1) * sizeof(char));
+  cur_string[lenBinStr] = '\0';
+  _rec_bin_string(lenBinStr, binarystring, cur_string, 0);
+
+  free(cur_string);
+}
+
+void _rec_bin_string(int lenBinStr, char* binarystring, char* cur_string, int index) {
+
+  if (binarystring[0] == '\0') {
+    print_ar(cur_string, lenBinStr);
+  } else {
+    if (binarystring[0] == '1' || binarystring[0] == '0') {
+      cur_string[index] = binarystring[0];
+      _rec_bin_string(lenBinStr, binarystring + 1, cur_string, index + 1);
+    } else {
+      cur_string[index] = '0';
+      _rec_bin_string(lenBinStr, binarystring + 1, cur_string, index + 1);
+      cur_string[index] = '1';
+      _rec_bin_string(lenBinStr, binarystring + 1, cur_string, index + 1);
+    }
+  }
+}
+void print_ar(char *ar, int len) {
+  for (int i = 0; i < len; ++i) {
+    printf("%c", ar[i]);
+  }
+  printf("\n");
+}
+
+
+
+
+int main(int argc, char* argv[]) {
+  int lenBinStr = strlen(argv[1]);
+  char* binaryString = (char*) malloc((lenBinStr + 1) * sizeof(char));
+  strcpy(binaryString, argv[1]);
+
+  rec_bin_string(lenBinStr, binaryString);
+  free(binaryString);
+
+
+
+  return 0;
+}
---
+++ C_samples/homework7.2/cmake-build-debug/CMakeFiles/homework7_2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/homework7_2.dir/main.c.o"
+  "homework7_2.pdb"
+  "homework7_2"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/homework7_2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
